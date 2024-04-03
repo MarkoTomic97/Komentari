00bf4999f432fb25a013a10f3930dd3805f97183
Marko Tomic <tomicmarko97@proton.me>
2024-04-03 16:08:06 +0200

    v0.0.2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,26 +11,41 @@ function App() {
     setComment(e.target.value);
   };
   return (
-    <div className="main-container">
-      {comments.map((text) => (
-        <div className="comment-container">{text}</div>
-      ))}
-      <div className="comment-flexbox">
-        <h4 className="comment-text" id="add-comm">Add a comment</h4>
-        <div className="comment-outerbox">
-          <h4 className="comment-text" id="write">Write</h4>
-          <textarea
-            value={comment}
-            onChange={onChangeHandler}
-            className="input-box"
-          />
-        </div>
-        
-        <button onClick={onClickHandler} className="comment-button">
-          Comment
-        </button>
+    <>
+      <div className="top-container">
+        <h2 className="issue-name">*Issue Name*</h2>
       </div>
-    </div>
+      <div className="main-container">
+        {comments.map((text) => (
+          <>
+            <div className="user-comment-container">
+              <div className="user-profile-pic"></div>
+              <div className="user-info"></div>
+            </div>
+            <div className="comment-container">{text}</div>
+          </>
+        ))}
+        <div className="comment-flexbox">
+          <div className="user-comment-container">
+            <div className="user-profile-pic"></div>
+            <h4 className="add-comm">Add a comment</h4>  
+          </div>
+          <div className="comment-outerbox">
+            <h4 className="comment-text" id="write">Write</h4>
+            <textarea
+              value={comment}
+              onChange={onChangeHandler}
+              className="input-box"
+            />
+          </div>
+
+          <button onClick={onClickHandler} className="comment-button">
+            Comment
+          </button>
+        </div>
+      </div>
+
+    </>
   );
 }
 
