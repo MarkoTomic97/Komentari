f796fafd75d146110a120dca58ef044e482b6c94
Marko Tomic <tomicmarko97@proton.me>
2024-04-04 17:09:11 +0200

    v0.0.3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,41 +10,61 @@ function App() {
   const onChangeHandler = (e) => {
     setComment(e.target.value);
   };
+
+  function checkInput() {
+    //var input = document.getElementById('textInput');
+    var button = document.getElementById('submitButton');
+    if(comment !== '')
+    {
+      button.disabled = false;
+    } else
+      button.disabled = true;
+  }
+
+
   return (
-    <>
-      <div className="top-container">
-        <h2 className="issue-name">*Issue Name*</h2>
-      </div>
-      <div className="main-container">
-        {comments.map((text) => (
-          <>
-            <div className="user-comment-container">
-              <div className="user-profile-pic"></div>
-              <div className="user-info"></div>
+    <>  
+        <div className="container">
+          <div className="top-container">
+            <h2 className="issue-name">*Issue Name*</h2>
+          </div>
+          <div className="main-container">
+            {
+              comments.map((text)=> (
+                <div className="comment-container">
+                  <div className="profile-pic"></div>
+                  <div className="triangle"></div>
+                  <div className="text-and-info-container">
+                    <div className="user-info-container"></div>
+                    <div className="text-container">{text}</div>
+                  </div>
+                </div>
+              )
+              )
+            }
+            <div className="comment-flexbox">
+              <div className="profile-pic" id="user-profile-pic"></div>
+              <div className="comment-outerbox">
+                <h4 className="add-comment">Add comment</h4>
+                <div className="comment-tabs">
+                  <h4 className="write">Write</h4>
+                </div>
+                <textarea
+                  value={comment}
+                  onChange={onChangeHandler}
+                  onInput={checkInput}
+                  className="input-box"
+                  id="textInput"
+                />
+                <button onClick={onClickHandler} className="comment-button"
+                id="submitButton">
+                  Comment
+                </button>
+              
+              </div>
             </div>
-            <div className="comment-container">{text}</div>
-          </>
-        ))}
-        <div className="comment-flexbox">
-          <div className="user-comment-container">
-            <div className="user-profile-pic"></div>
-            <h4 className="add-comm">Add a comment</h4>  
           </div>
-          <div className="comment-outerbox">
-            <h4 className="comment-text" id="write">Write</h4>
-            <textarea
-              value={comment}
-              onChange={onChangeHandler}
-              className="input-box"
-            />
-          </div>
-
-          <button onClick={onClickHandler} className="comment-button">
-            Comment
-          </button>
         </div>
-      </div>
-
     </>
   );
 }
