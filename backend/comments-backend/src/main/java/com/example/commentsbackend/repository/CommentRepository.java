4d37f063eea442a3e11194ab3bfcfbc811a5064c
Marko Tomic <tomicmarko97@proton.me>
2024-04-11 18:53:50 +0200

    Sredjen folder

--- backend/comments-backend/src/main/java/com/example/commentsbackend/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.commentsbackend.repository;
-
-import com.example.commentsbackend.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
